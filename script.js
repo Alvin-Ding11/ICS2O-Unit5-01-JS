4645f721a4a64506d532810b32c779d02f1cbd6e
AlvinDing11 <alvin.ding@ocsbstudent.ca>
2021-03-16 13:45:50 +0000

    semicolon removal

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.write('Hello, World')
 
-var img = document.createElement("img");
+var img = document.createElement('img)
 
-img.src = "./images/Hello World.png";
-var src = document.getElementById("x");
+img.src = './images/Hello World.png'
+var src = document.getElementById('x')
 
-src.appendChild(img);
+src.appendChild(img)
